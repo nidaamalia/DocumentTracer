85091f07aeed2cfb7f92a18dbce44b65aea4ab67
Nida Amalia <Nida Amalia>
2016-11-21 07:12:01 +0700

    multilevel dropdown

--- application/views/aside.php
+++ application/views/aside.php
@@ -6,9 +6,16 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">        
         <li class="treeview">
-          <a href="<?php echo base_url();?>beranda">
+          <a href="#">
             <i class="fa fa-file-text-o"></i> <span>Lihat Data</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
           </a>
+          <ul class="treeview-menu">
+            <li><a href="index.html"><i class="fa fa-circle-o"></i> Dokumen Masuk</a></li>
+            <li class="active"><a href="index2.html"><i class="fa fa-circle-o"></i> Dokumen Keluar</a></li>
+          </ul>          
         </li>
         <li class="treeview">
           <a href="#">
@@ -24,10 +31,57 @@
         </li>
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-sign-out"></i> <span>Keluar</span>
+            <i class="fa fa-eye"></i> <span>Panduan</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
           </a>
-        </li> 
-        
+          <ul class="treeview-menu">            
+            <li>
+              <a href="#"><i class="fa fa-circle-o"></i> DAEK
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">                
+                <li>
+                  <a href="#"><i class="fa fa-circle-o"></i> FA
+                    <span class="pull-right-container">
+                      <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                  </a>
+                  <ul class="treeview-menu">
+                    <li><a href="#"><i style="color: red" class="fa fa-circle-o"></i> Merah</a></li>
+                    <li><a href="#"><i style="color: yellow" class="fa fa-circle-o"></i> Kuning</a></li>
+                    <li><a href="#"><i style="color: green" class="fa fa-circle-o"></i> Hijau</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>            
+            <li>
+              <a href="#"><i class="fa fa-circle-o"></i> DPE
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">                
+                <li>
+                  <a href="#"><i class="fa fa-circle-o"></i> FA
+                    <span class="pull-right-container">
+                      <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                  </a>
+                  <ul class="treeview-menu">
+                    <li><a href="#"><i style="color: red" class="fa fa-circle-o"></i> Merah</a></li>
+                    <li><a href="#"><i style="color: yellow" class="fa fa-circle-o"></i> Kuning</a></li>
+                    <li><a href="#"><i style="color: green" class="fa fa-circle-o"></i> Hijau</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>            
+          </ul>
+
+        </li>                
       </ul>
     </section>
     <!-- /.sidebar -->
